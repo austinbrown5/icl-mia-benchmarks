1b43630674e87d5e096f0c384be7717117a702ed
Austin Brown <austinb50121@gmail.com>
2024-09-26 20:31:42 -0400

    fix main

--- main.py
+++ main.py
@@ -1,40 +1,120 @@
 from vllm import LLM, SamplingParams
+from datasets import load_dataset
+import nltk
+from nltk import word_tokenize
+from nltk.tag import StanfordPOSTagger
+import random
 import numpy as np
+from prompts import Prompt
 from sklearn.metrics import roc_auc_score, roc_curve
 from scipy import stats
 from rouge_score import rouge_scorer
-import json
+import os
+import ssl
+import time
+
 
 def main():
     # Usage
     model_name = "meta-math/MetaMath-Mistral-7B"
+    dataset_name = "meta-math/MetaMathQA"
 
-    llm = LLM(model=model_name, dtype="half")
+    # load data
+    meta_math_ds = load_dataset(dataset_name, streaming = True) # load subset
+    # meta_math_ds = meta_math_ds.take(1000) # take only the first 1000 examples while debugging
+    meta_math_ds['train'] = meta_math_ds['train'].take(1000)
+    print('Dataset has been loaded')
+
+    # llm = LLM(model=model_name, dtype="half")
+    llm = LLM(model=model_name, dtype="half", max_model_len=30000) #decrease model max length to fit in kv cache for vllm
     sampling_params = SamplingParams(temperature=0.7, top_p=0.95)
 
-    with open('prompts.json', 'r') as f:
-        prompts = json.load(f)
-    with open('targets.json', 'r') as f:
-        targets = json.load(f)
+    meta_math_training = meta_math_ds['train']
+
+    # loop through the data and generate our different prompts 
+    prompts = {
+        "general_prompts": [],
+        "guided_prompts": [],
+        "ts_prompts": [],
+        "standard_queries": []
+    }
+
+    targets = {
+        "guided": [],
+        "ts": [],
+        "answers": []
+    }
     
-    general_outputs = llm.generate(prompts['general_promts'], sampling_params)
+    # load stanford pos tagger, need to change to not hard code paths
+    print('Loading POS Tagger...')
+    os.environ['CLASSPATH']="/usr/project/xtmp/arb153/icl-mia-benchmarks/stanford-postagger-full-2020-11-17/stanford-postagger.jar"
+    os.environ["STANFORD_MODELS"] = "/usr/project/xtmp/arb153/icl-mia-benchmarks/stanford-postagger-full-2020-11-17/models"
+    tagger = StanfordPOSTagger('english-left3words-distsim.tagger')
+    print('POS Tagger Loaded')
+
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
+
+    nltk.download('punkt_tab')
+
+    
+    #going to go through the dataset and generate our unique prompts, do not need these for our min_k, perplexity, or cdd attacks
+    print('Generating Guided Prompting and TS-Guessing Prompts...')
+
+    start_time = time.time()
+    for data_point in meta_math_training:
+
+        #split for guided prompting
+        splits = guided_prompt_split_fn(data_point, 'query')
+
+        guided_prompt_insert = splits['guided_prompt_part_1']
+        guided_prompt_target = splits['guided_prompt_part_2']
+
+        prompt = Prompt()
+        guided_prompt = prompt.get_prompt("guided").format(dataset_name = dataset_name,
+                                                           first_piece = guided_prompt_insert)
+        general_prompt = prompt.get_prompt("general").format(first_piece = guided_prompt_insert)
+
+        #ts guessing prompt and target generation, question based method
+        ts_prompt, ts_target = ts_guessing_prompt(data_point, tagger, 'query')
+
+        prompts['general_prompts'].append(general_prompt)
+        prompts['guided_prompts'].append(guided_prompt)
+        
+        prompts['ts_prompts'].append(ts_prompt)
+
+        prompts['standard_queries'].append(data_point['query'])
+
+        targets['guided'].append(guided_prompt_target)
+        targets['ts'].append(ts_target)
+        targets['answers'].append(data_point['response'])
+    
+    print(f'Prompts Generated. Run time was {time.time() - start_time}')
+
+    print('Performing Guided Prompting attack...')
+    general_outputs = llm.generate(prompts['general_prompts'], sampling_params)
     guided_outputs = llm.generate(prompts['guided_prompts'], sampling_params)
-
-    ts_outputs = llm.generate(prompts['ts_prompts'], sampling_params)
-
     scorer = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=True)
-
     general_scores = [scorer.score(ref, hyp)['rougeL'].fmeasure for ref, hyp in zip(targets['answers'], [output.outputs[0].text for output in general_outputs])]
     guided_scores = [scorer.score(ref, hyp)['rougeL'].fmeasure for ref, hyp in zip(targets['guided'], [output.outputs[0].text for output in guided_outputs])]
+     # t_statistic, p_value = stats.ttest_rel(guided_scores, general_scores)
+    # guided_scores = [1 if p_value < 0.05 else 0 for _ in range(len(guided_scores))] # change to use p-value as membership score ( try p value, 1 - p value)
+    gp_scores = [guided - general for guided, general in zip(guided_scores, general_scores)]
 
-    t_statistic, p_value = stats.ttest_rel(guided_scores, general_scores)
 
-    guided_scores = [1 if p_value < 0.05 else 0 for _ in range(len(guided_scores))] # change to use p-value as membership score ( try p value, 1 - p value)
-
+    print('Performing TS Guessing attack...')
+    ts_outputs = llm.generate(prompts['ts_prompts'], sampling_params)        
     ts_scores = [1 if output.outputs[0].text.strip().lower() == target.strip().lower() else 0 
                  for output, target in zip(ts_outputs, targets['ts'])]
 
+    print('Performing CDD attack...')
     cdd_scores = cdd(prompts= prompts['standard_queries'], llm= llm, alpha = 0.05, xi = 0.01 ) #this will be binary 1s and 0s
+    
+    print('Performing Min K attack...')
     min_k_scores, loss_scores = min_k_loss(prompts['standard_queries'], llm, k_percent= 10)
 
     truths = np.ones(len(prompts)) # we know the model has been contaminated
@@ -42,11 +122,11 @@ def main():
     assert len(truths) == len(cdd_scores)
     assert len(truths) == len(min_k_scores)
     assert len(truths) == len(loss_scores)
-    assert len(truths) == len(guided_scores)
+    assert len(truths) == len(gp_scores)
     assert len(truths) == len(ts_scores)
 
     #scoring
-    aucroc_guided = roc_auc_score(truths, guided_scores)
+    aucroc_guided = roc_auc_score(truths, gp_scores)
     aucroc_ts = roc_auc_score(truths, ts_scores)
     aucroc_cdd = roc_auc_score(truths, cdd_scores)
     aucroc_min_k = roc_auc_score(truths, min_k_scores)
@@ -57,7 +137,7 @@ def main():
     print(f"AUCROC Min-k: {aucroc_min_k}")
 
     for fpr_threshold in [0.01, 0.05, 0.10, 0.25]:
-        tpr_guided = tpr_at_fpr(truths, guided_scores, fpr_threshold)
+        tpr_guided = tpr_at_fpr(truths, gp_scores, fpr_threshold)
         tpr_ts = tpr_at_fpr(truths, ts_scores, fpr_threshold)
         tpr_cdd = tpr_at_fpr(truths, cdd_scores, fpr_threshold)
         tpr_min_k = tpr_at_fpr(truths, min_k_scores, fpr_threshold)
@@ -71,6 +151,56 @@ def tpr_at_fpr(y_true, y_score, fpr_threshold):
     fpr, tpr, _ = roc_curve(y_true, y_score)
     index = next(i for i, x in enumerate(fpr) if x >= fpr_threshold)
     return tpr[index]
+
+def guided_prompt_split_fn(example, text_key):
+    splits = {'guided_prompt_part_1': '', 'guided_prompt_part_2': ''}
+    text = example[text_key]
+    sentences = nltk.sent_tokenize(text, )
+
+    if len(sentences) == 1:
+        # Split the single sentence in half
+        mid_point = len(text) // 2
+        splits['guided_prompt_part_1'] = text[:mid_point]
+        splits['guided_prompt_part_2'] = text[mid_point:]
+    elif len(sentences) > 1:
+        # Original logic for multiple sentences
+        first_part_length = random.randint(1, len(sentences) - 1)
+        splits['guided_prompt_part_1'] = ' '.join(sentences[:first_part_length])
+        splits['guided_prompt_part_2'] = ' '.join(sentences[first_part_length:])
+    else:
+        # Handle empty text case
+        splits['guided_prompt_part_1'] = ''
+        splits['guided_prompt_part_2'] = ''
+
+    return splits
+
+def ts_guessing_prompt(
+    example, 
+    tagger,
+    text_key,
+    type_hint=False,
+):
+    #question based prompt generation for ts guessing
+    text = example[text_key]
+    tags = tagger.tag(text.split())
+    words = [x for x in tags if x[1] in ['NN', 'JJ', 'VB']]
+    if len(words) == 0:
+        return "failed", ""
+    idx = np.random.randint(len(words))
+    word = words[idx][0]
+    for i in range(len(text)-len(word)+1):
+        if text[i:(i+len(word))] == word:
+            text = text[:i] + "[MASK]" + text[(i+len(word)):]
+            break
+
+    prompt = "Complete the sentence in one word:"
+    prompt += f"\n\n{text}"
+    if type_hint:
+        example_type = example["type"]
+        prompt += f"\nHint: {example_type}"
+    prompt += "\nReply the answer only."
+
+    return prompt, word
     
 def get_ll_vllm(sentence, llm):
     # we can only get the logprobs for up to the top 100 tokens with vllm
@@ -130,13 +260,11 @@ def cdd(prompts, llm, alpha = 0.05, xi = 0.01):
         s_0 = greedy_sample.text
 
         peak = get_peak(generated_texts, s_0, alpha)
-        # is_contaminated = peak > xi
-        # if is_contaminated:
-        #     cdd_scores.append(1)
-        # else:
-        #     cdd_scores.append(0)
-        cdd_scores.append(peak) #* we want a membership score instead of a binary value, 
-        #? these scores will be very low, is that alright
+        is_contaminated = peak / len(generated_texts) > xi
+        if is_contaminated:
+            cdd_scores.append(1)
+        else:
+            cdd_scores.append(0)
     return cdd_scores
 
 def levenshtein_distance(s1, s2):
@@ -166,5 +294,5 @@ def get_peak(samples, s_0, alpha):
 
     return peak
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
